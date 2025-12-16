fd677d3f27409360859a49b61ab48140129378f6
giorgia <n00230352@iadt.ie>
2025-12-16 23:13:06 +0000

    doctor index edit

--- src/components/DeleteBtn.jsx
+++ src/components/DeleteBtn.jsx
@@ -3,57 +3,80 @@ import { Trash } from "lucide-react";
 import axios from "@/config/api";
 import { useState } from "react";
 
+import {
+	AlertDialog,
+	AlertDialogTrigger,
+	AlertDialogContent,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogCancel,
+	AlertDialogAction,
+} from "@/components/ui/alert-dialog";
+
 export default function DeleteBtn({ resource, id, onDeleteCallback }) {
-    const [isDeleting, setIsDeleting] = useState(false);
+	const [isDeleting, setIsDeleting] = useState(false);
 
-    let token = localStorage.getItem('token');
+	let token = localStorage.getItem("token");
 
-    const onDelete = async () => {
-        const options = {
-            method: "DELETE",
-            url: `/${resource}/${id}`,
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-      };
+	const onDelete = async () => {
+		const options = {
+			method: "DELETE",
+			url: `/${resource}/${id}?cascade=true`,
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		};
 
-      try {
-        let response = await axios.request(options);
-        console.log(response.data);
-        if (onDeleteCallback) {
-            onDeleteCallback(id);
-        }
-        
-      } catch (err) {
-        console.log(err);
-      }
-    };
+		try {
+			let response = await axios.request(options);
+			console.log(response.data);
+			if (onDeleteCallback) {
+				onDeleteCallback(id);
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-  return (
-    (!isDeleting) ?(
-        <Button 
-            className="cursor-pointer text-red-500 hover:border-red-700 hover:text-red-700"
-            variant="outline"
-            size="icon"
-            onClick={() => setIsDeleting(true)}
-        ><Trash /></Button>
-    ) : (
-        <>
-            <p>Are you sure?</p>
-            <Button 
-                onClick={onDelete}
-                variant="outline"
-                size="sm"
-                className="cursor-pointer text-red-500 border-red-500 hover:text-red-700 hover:border-red-700"
-            >Yes</Button>
-            <Button 
-                onClick={() => setIsDeleting(false)}
-                variant="outline"
-                size="sm"
-                className="cursor-pointer text-slate-500 border-slate-500 hover:text-slate-700 hover:border-slate-700"
-            >No</Button>
-        </>
-    )
-   
-  );
+	return (
+		<AlertDialog open={isDeleting} onOpenChange={setIsDeleting}>
+			<AlertDialogTrigger asChild>
+				<Button
+					className="cursor-pointer text-red-500 hover:border-red-700 hover:text-red-700"
+					variant="outline"
+					size="icon"
+					onClick={() => setIsDeleting(true)}
+				>
+					<Trash />
+				</Button>
+			</AlertDialogTrigger>
+
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>Are you sure?</AlertDialogTitle>
+					<AlertDialogDescription>
+						This action cannot be undone.
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+
+				<AlertDialogFooter>
+					<AlertDialogAction
+						onClick={onDelete}
+						className="cursor-pointer text-red-500 border border-red-500 hover:text-red-700 hover:border-red-700 bg-transparent"
+					>
+						Yes
+					</AlertDialogAction>
+
+					<AlertDialogCancel
+						onClick={() => setIsDeleting(false)}
+						className="cursor-pointer text-slate-500 border border-slate-500 hover:text-slate-700 hover:border-slate-700"
+					>
+						No
+					</AlertDialogCancel>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	);
 }
--- src/pages/doctors/Index.jsx
+++ src/pages/doctors/Index.jsx
@@ -2,104 +2,137 @@ import { useEffect, useState } from "react";
 import axios from "@/config/api";
 import { Link, useNavigate } from "react-router";
 import { Button } from "@/components/ui/button";
-import { Eye, Pencil } from "lucide-react";
+import { Eye, Pencil, Phone, Plus } from "lucide-react";
 import DeleteBtn from "@/components/DeleteBtn";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
+	Table,
+	TableBody,
+	TableCaption,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
 } from "@/components/ui/table";
 import { toast } from "sonner";
 
 export default function Index() {
-    const [doctors, setDoctors] = useState([]);
+	const [doctors, setDoctors] = useState([]);
 
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    useEffect(() => {
-    const fetchDoctors = async () => {
-        const options = {
-        method: "GET",
-        url: "/doctors",
-        };
+	useEffect(() => {
+		const fetchDoctors = async () => {
+			const options = {
+				method: "GET",
+				url: "/doctors",
+			};
 
-        try {
-            let response = await axios.request(options);
-            console.log(response.data);
-            setDoctors(response.data);
-        } catch (err) {
-            console.log(err);
-        }
-    };
+			try {
+				let response = await axios.request(options);
+				console.log(response.data);
+				setDoctors(response.data);
+			} catch (err) {
+				console.log(err);
+			}
+		};
 
-    fetchDoctors();
+		fetchDoctors();
 
-    console.log("Hello");
-    }, []);
+		console.log("Hello");
+	}, []);
 
-    const onDeleteCallback = (id) => {
-    toast.success("Doctor deleted successfully");
-    setDoctors(doctors.filter(doctor => doctor.id !== id));
-  
-  };
+	const onDeleteCallback = (id) => {
+		toast.success("Doctor deleted successfully");
+		setDoctors(doctors.filter((doctor) => doctor.id !== id));
+	};
 
-    return (
-        <>
-      <Button
-        asChild
-        variant='outline'
-        className='mb-4 mr-auto block'
-      ><Link size='sm' to={`/doctors/create`}>Create New Doctor</Link>
-      </Button>
+	return (
+		<>
+    <div className="flex items-center justify-between">
+			<h2 className="mb-4 ">Manage all doctors</h2>
+			<Button asChild variant="outline" className="mb-4">
+				<Link to="/doctors/create" className="inline-flex items-center gap-2">
+					<Plus className="h-4 w-4" />
+					New Doctor
+				</Link>
+			</Button>
+</div>
+			<Table>
+				<TableCaption>A list of your recent invoices.</TableCaption>
+				<TableHeader>
+					<TableRow>
+						<TableHead>Doctor</TableHead>
+						<TableHead>Contact</TableHead>
+						<TableHead>Specialisation</TableHead>
+						<TableHead>Actions</TableHead>
+						<TableHead></TableHead>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					{doctors.map((doctor) => (
+						<TableRow key={doctor.id}>
+							<TableCell>
+								<div className="flex items-center gap-2">
+									<Avatar className="h-8 w-8">
+										<AvatarImage
+											src={doctor.avatarUrl}
+											alt={`${doctor.first_name} ${doctor.last_name}`}
+										/>
+										<AvatarFallback>
+											{doctor.first_name[0]}
+											{doctor.last_name[0]}
+										</AvatarFallback>
+									</Avatar>
+									<span>
+										Dr. {doctor.first_name} {doctor.last_name}
+									</span>
+								</div>
+							</TableCell>
 
+							<TableCell>
+								{doctor.email}
+								<br />
+								<a
+									href={`tel:${doctor.phone}`}
+									className="flex items-center gap-1 text-sm hover:text-blue-600"
+								>
+									<Phone className="h-4 w-4" />
+									<span>{doctor.phone}</span>
+								</a>
+							</TableCell>
 
-    <Table>
-      <TableCaption>A list of your recent invoices.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead>First Name</TableHead>
-          <TableHead>Last Name</TableHead>
-          <TableHead>Email</TableHead>
-          <TableHead>Phone</TableHead>
-          <TableHead>Specialisation</TableHead>
-          <TableHead></TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {doctors.map((doctor) => (
-          <TableRow key={doctor.id}>
-            <TableCell>{doctor.first_name}</TableCell>
-            <TableCell>{doctor.last_name}</TableCell>
-            <TableCell>{doctor.email}</TableCell>
-            <TableCell>{doctor.phone}</TableCell>
-            <TableCell>{doctor.specialisation}</TableCell>
-            <TableCell>
-              <div className="flex gap-2">
-              <Button 
-                className="cursor-pointer hover:border-blue-500"
-                variant="outline"
-                size="icon"
-                onClick={() => navigate(`/doctors/${doctor.id}`)}
-              ><Eye /></Button>
-              <Button 
-                className="cursor-pointer hover:border-blue-500"
-                variant="outline"
-                size="icon"
-                onClick={() => navigate(`/doctors/${doctor.id}/edit`)}
-              ><Pencil /></Button>
-              <DeleteBtn onDeleteCallback={onDeleteCallback} resource="doctors" id={doctor.id} />
-              </div>
-
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-    </>
-    );
-};
+							<TableCell>{doctor.specialisation}</TableCell>
+							<TableCell>
+								<div className="flex gap-2">
+									<Button
+										className="cursor-pointer hover:border-blue-500"
+										variant="outline"
+										size="icon"
+										onClick={() => navigate(`/doctors/${doctor.id}`)}
+									>
+										<Eye />
+									</Button>
+									<Button
+										className="cursor-pointer hover:border-blue-500"
+										variant="outline"
+										size="icon"
+										onClick={() => navigate(`/doctors/${doctor.id}/edit`)}
+									>
+										<Pencil />
+									</Button>
+									<DeleteBtn
+										onDeleteCallback={onDeleteCallback}
+										resource="doctors"
+										id={doctor.id}
+									/>
+								</div>
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</>
+	);
+}
